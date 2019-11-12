18db7a84b594a384f4d3ac95e5e77afa779e23dc
Xintong Song <tonysong820@gmail.com>
2019-11-12 20:01:30 +0800

    Revert "[hotfix][core][config] Change default value of task off-heap memory size to 1MB."
    This reverts commit 04d36a4b50c1c95264f0f4b37ed2764df0880545.

--- flink-core/src/main/java/org/apache/flink/configuration/TaskManagerOptions.java
+++ flink-core/src/main/java/org/apache/flink/configuration/TaskManagerOptions.java
@@ -303,7 +303,7 @@ public class TaskManagerOptions {
 	 */
 	public static final ConfigOption<String> TASK_OFF_HEAP_MEMORY =
 		key("taskmanager.memory.task.off-heap.size")
-			.defaultValue("1m")
+			.defaultValue("0b")
 			.withDescription("Task Heap Memory size for TaskExecutors. This is the size of off heap memory (JVM direct"
 				+ " memory or native memory) reserved for user code.");
 
