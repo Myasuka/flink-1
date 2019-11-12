4def7302330a8a3df33d5b410483440f90eebc09
Zhu Zhu <reedpor@gmail.com>
2019-11-12 12:18:54 +0800

    [FLINK-14594][runtime] Replace ResourceProfile#equals usages with #hasSameResources for resource checking
    #equals should be used for strict comparison only.

--- flink-runtime/src/main/java/org/apache/flink/runtime/resourcemanager/slotmanager/SlotManagerImpl.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/resourcemanager/slotmanager/SlotManagerImpl.java
@@ -626,7 +626,7 @@ public class SlotManagerImpl implements SlotManager {
 	@Nullable
 	private PendingTaskManagerSlot findExactlyMatchingPendingTaskManagerSlot(ResourceProfile resourceProfile) {
 		for (PendingTaskManagerSlot pendingTaskManagerSlot : pendingSlots.values()) {
-			if (pendingTaskManagerSlot.getResourceProfile().equals(resourceProfile)) {
+			if (pendingTaskManagerSlot.getResourceProfile().hasSameResources(resourceProfile)) {
 				return pendingTaskManagerSlot;
 			}
 		}
