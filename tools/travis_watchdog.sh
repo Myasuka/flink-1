fff5a193f7d24e0b57f6c950d005176693dda8f4
Chesnay Schepler <chesnay@apache.org>
2019-11-14 13:49:35 +0100

    [FLINK-11468][tests] Setup surefire execution

--- tools/travis_watchdog.sh
+++ tools/travis_watchdog.sh
@@ -67,6 +67,7 @@ MVN_TEST_OPTIONS="-Dflink.tests.with-openssl"
 
 MVN_COMPILE="mvn $MVN_COMMON_OPTIONS $MVN_COMPILE_OPTIONS $PROFILE $MVN_COMPILE_MODULES install"
 MVN_TEST="mvn $MVN_COMMON_OPTIONS $MVN_TEST_OPTIONS $PROFILE $MVN_TEST_MODULES verify"
+MVN_E2E="mvn $MVN_COMMON_OPTIONS $MVN_TEST_OPTIONS $PROFILE -Dcategories="org.apache.flink.tests.util.categories.PreCommit" flink-end-to-end-tests verify"
 
 MVN_PID="${ARTIFACTS_DIR}/watchdog.mvn.pid"
 MVN_EXIT="${ARTIFACTS_DIR}/watchdog.mvn.exit"
@@ -264,7 +265,7 @@ case $TEST in
     (misc)
         if [ $EXIT_CODE == 0 ]; then
             printf "\n\n==============================================================================\n"
-            printf "Running end-to-end tests\n"
+            printf "Running bash end-to-end tests\n"
             printf "==============================================================================\n"
 
             FLINK_DIR=build-target flink-end-to-end-tests/run-pre-commit-tests.sh
@@ -272,8 +273,20 @@ case $TEST in
             EXIT_CODE=$?
         else
             printf "\n==============================================================================\n"
-            printf "Previous build failure detected, skipping end-to-end tests.\n"
+            printf "Previous build failure detected, skipping bash end-to-end tests.\n"
             printf "==============================================================================\n"
+        fi
+        if [ $EXIT_CODE == 0 ]; then
+            printf "\n\n==============================================================================\n"
+            printf "Running java end-to-end tests\n"
+            printf "==============================================================================\n"
+
+            run_with_watchdog "$MVN_E2E"
+
+            EXIT_CODE=$?
+        else
+            printf "\n==============================================================================\n"
+            printf "Previous build failure detected, skipping java end-to-end tests.\n"
         fi
     ;;
 esac
