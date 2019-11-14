e4e45953ce0e5f3a20040e7bdf0016f90a0ea76e
Zhu Zhu <reedpor@gmail.com>
2019-11-14 23:29:33 +0800

    [FLINK-14062][runtime] Calculate managed memory fraction based on slot sharing groups

--- flink-streaming-java/src/main/java/org/apache/flink/streaming/api/graph/StreamingJobGraphGenerator.java
+++ flink-streaming-java/src/main/java/org/apache/flink/streaming/api/graph/StreamingJobGraphGenerator.java
@@ -71,14 +71,17 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.IdentityHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
 import static org.apache.flink.runtime.jobgraph.tasks.CheckpointCoordinatorConfiguration.MINIMAL_CHECKPOINT_TIME;
+import static org.apache.flink.util.Preconditions.checkArgument;
 
 /**
  * The StreamingJobGraphGenerator converts a {@link StreamGraph} into a {@link JobGraph}.
@@ -162,6 +165,8 @@ public class StreamingJobGraphGenerator {
 		setPhysicalEdges();
 
 		setSlotSharingAndCoLocation();
+
+		setManagedMemoryFraction();
 
 		configureCheckpointing();
 
@@ -682,6 +687,96 @@ public class StreamingJobGraphGenerator {
 				constraint.f1.addVertex(vertex);
 			}
 		}
+	}
+
+	private void setManagedMemoryFraction() {
+		// all slot sharing groups in this job
+		final Set<SlotSharingGroup> slotSharingGroups = Collections.newSetFromMap(new IdentityHashMap<>());
+		// maps a job vertex ID to its head operator ID
+		final Map<JobVertexID, Integer> vertexHeadOperators = new HashMap<>();
+		// maps a job vertex ID to IDs of all operators in the vertex
+		final Map<JobVertexID, Set<Integer>> vertexOperators = new HashMap<>();
+
+		for (Entry<Integer, JobVertex> entry : jobVertices.entrySet()) {
+			final int headOperatorId = entry.getKey();
+			final JobVertex jobVertex = entry.getValue();
+
+			final SlotSharingGroup jobVertexSlotSharingGroup = jobVertex.getSlotSharingGroup();
+			if (jobVertexSlotSharingGroup != null) {
+				slotSharingGroups.add(jobVertexSlotSharingGroup);
+			}
+
+			vertexHeadOperators.put(jobVertex.getID(), headOperatorId);
+
+			final Set<Integer> operatorIds = new HashSet<>();
+			operatorIds.add(headOperatorId);
+			operatorIds.addAll(chainedConfigs.getOrDefault(headOperatorId, new HashMap<>()).keySet());
+			vertexOperators.put(jobVertex.getID(), operatorIds);
+		}
+
+		for (SlotSharingGroup slotSharingGroup : slotSharingGroups) {
+			setManagedMemoryFractionForSlotSharingGroup(slotSharingGroup, vertexHeadOperators, vertexOperators);
+		}
+	}
+
+	private void setManagedMemoryFractionForSlotSharingGroup(
+			final SlotSharingGroup slotSharingGroup,
+			final Map<JobVertexID, Integer> vertexHeadOperators,
+			final Map<JobVertexID, Set<Integer>> vertexOperators) {
+
+		int groupOperatorCount = 0;
+		for (JobVertexID jobVertexID : slotSharingGroup.getJobVertexIds()) {
+			final Set<Integer> operatorIds = vertexOperators.get(jobVertexID);
+			groupOperatorCount += operatorIds.size();
+		}
+
+		for (JobVertexID jobVertexID : slotSharingGroup.getJobVertexIds()) {
+			for (int operatorNodeId : vertexOperators.get(jobVertexID)) {
+				final StreamConfig operatorConfig = vertexConfigs.get(operatorNodeId);
+				final ResourceSpec operatorResourceSpec = streamGraph.getStreamNode(operatorNodeId).getMinResources();
+				setManagedMemoryFractionForOperator(
+					operatorResourceSpec,
+					slotSharingGroup.getResourceSpec(),
+					groupOperatorCount,
+					operatorConfig);
+			}
+
+			// need to refresh the chained task configs
+			final int headOperatorNodeId = vertexHeadOperators.get(jobVertexID);
+			final StreamConfig vertexConfig = vertexConfigs.get(headOperatorNodeId);
+			vertexConfig.setTransitiveChainedTaskConfigs(chainedConfigs.get(headOperatorNodeId));
+		}
+	}
+
+	private void setManagedMemoryFractionForOperator(
+			final ResourceSpec operatorResourceSpec,
+			final ResourceSpec groupResourceSpec,
+			final int groupOperatorCount,
+			final StreamConfig operatorConfig) {
+
+		final double managedMemoryFractionOnHeap;
+		final double managedMemoryFractionOffHeap;
+
+		if (groupResourceSpec.equals(ResourceSpec.UNKNOWN)) {
+			checkArgument(groupOperatorCount > 0, "A slot sharing group must contain at least 1 operator");
+
+			managedMemoryFractionOnHeap = 1.0 / groupOperatorCount;
+			managedMemoryFractionOffHeap = 1.0 / groupOperatorCount;
+		} else {
+			final long groupOnHeapManagedMemoryBytes = groupResourceSpec.getOnHeapManagedMemory().getBytes();
+			final long groupOffHeapManagedMemoryBytes = groupResourceSpec.getOffHeapManagedMemory().getBytes();
+
+			managedMemoryFractionOnHeap = groupOnHeapManagedMemoryBytes > 0 ?
+				(double) operatorResourceSpec.getOnHeapManagedMemory().getBytes() / groupOnHeapManagedMemoryBytes :
+				0.0;
+
+			managedMemoryFractionOffHeap = groupOffHeapManagedMemoryBytes > 0 ?
+				(double) operatorResourceSpec.getOffHeapManagedMemory().getBytes() / groupOffHeapManagedMemoryBytes :
+				0.0;
+		}
+
+		operatorConfig.setManagedMemoryFractionOnHeap(managedMemoryFractionOnHeap);
+		operatorConfig.setManagedMemoryFractionOffHeap(managedMemoryFractionOffHeap);
 	}
 
 	private void configureCheckpointing() {
